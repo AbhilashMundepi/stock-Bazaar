c831591879386f460be96ec945f8d4c07628ff3f
Abhilash Mundepi <160834131+AbhilashMundepi@users.noreply.github.com>
2025-11-14 21:25:33 +0530

    Change login API endpoint to production URL

--- frontend/src/landing_page/Pages/Login.jsx
+++ frontend/src/landing_page/Pages/Login.jsx
@@ -33,7 +33,7 @@ const  Login = () => {
     e.preventDefault();
     try {
       const { data } = await axios.post(
-        "http://localhost:3002/login",
+        "https://stock-bazaar-1.onrender.com",
         {
           ...inputValue,
         },
