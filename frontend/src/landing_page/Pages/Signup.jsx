e04d19dba446f22991c96f6fcdd6c4d0e4562235
Abhilash Mundepi <160834131+AbhilashMundepi@users.noreply.github.com>
2025-11-14 21:26:23 +0530

    Update signup API endpoint to production URL

--- frontend/src/landing_page/Pages/Signup.jsx
+++ frontend/src/landing_page/Pages/Signup.jsx
@@ -34,7 +34,7 @@ const Signup =() => {
     e.preventDefault();
     try {
       const { data } = await axios.post(
-        "http://localhost:3002/signup",
+        "https://stock-bazaar-1.onrender.com",
         {
           ...inputValue,
         },
